92686e5fa3f1aa910404179242636d7d2ed415a7
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2023-11-28 13:23:15 +0200

    initial commit API

---
+++ src/main/java/com/virtualarena/api/VirtualArenaApiApplication.java
@@ -0,0 +1,13 @@
+package com.virtualarena.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualArenaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualArenaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/virtualarena/api/VirtualArenaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtualarena.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualArenaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
