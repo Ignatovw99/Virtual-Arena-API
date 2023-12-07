7a3b047fd0835f164d733f01e7b6046899c0ac7b
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2023-12-07 14:40:52 +0200

    oauth2 resource server security config extended

--- src/main/java/com/virtualarena/api/config/SecurityConfig.java
+++ src/main/java/com/virtualarena/api/config/SecurityConfig.java
@@ -1,22 +1,65 @@
 package com.virtualarena.api.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.virtualarena.api.exception.ErrorDetails;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final ObjectMapper objectMapper;
     @Value("${virtual-arena.auth.access-token.subject}")
     private String accessTokenSubject;
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests.anyRequest().authenticated())
+                .oauth2ResourceServer(resourceServer ->
+                    resourceServer.jwt(Customizer.withDefaults()))
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(authenticationEntryPoint()))
+                .build();
+    }
 
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
         converter.setPrincipalClaimName(accessTokenSubject);
         return converter;
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authenticationException) -> {
+            ErrorDetails errorDetails = new ErrorDetails(
+                    LocalDateTime.now(),
+                    authenticationException.getMessage(),
+                    request.getRequestURI(),
+                    "UNAUTHORIZED"
+            );
+
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write(objectMapper.writeValueAsString(errorDetails));
+            response.getWriter().flush();
+        };
     }
 }
