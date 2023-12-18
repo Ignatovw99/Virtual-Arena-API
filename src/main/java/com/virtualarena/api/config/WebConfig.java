1ae069ada2b45972b74b0568a3409bb6289e0af4
Lyuboslav Ignatov <lyuboslavw@gmail.com>
2023-12-18 09:33:01 +0200

    env vars added to configuration props

--- src/main/java/com/virtualarena/api/config/WebConfig.java
+++ src/main/java/com/virtualarena/api/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:[*]")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
                         .exposedHeaders("*")
                         .allowedHeaders("*");
